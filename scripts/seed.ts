a82e47d8774a40ee3a14d804efc2ec49bf4b728f
mostafa709 <mostafa.hesham7072@gmail.com>
2024-12-21 15:15:33 +0300

    publish done but you need add descound price and viemo and problem with clreck

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -6,13 +6,9 @@ async function main() {
   try {
     await database.category.createMany({
       data: [
-        { name: "Computer Science" },
-        { name: "Music" },
-        { name: "Fitness" },
-        { name: "Photography" },
-        { name: "Accounting" },
-        { name: "Engineering" },
-        { name: "Filming" },
+        { name: "Basic" },
+        { name: "Medium" },
+        { name: "Advanced" },
       ]
     });
 
